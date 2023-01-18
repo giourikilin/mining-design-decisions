cff9feaba16a275ec58af4029c1a8f9130c834d5
Jesse Maarleveld <jessemaarleveld@gmail.com>
2023-01-18 13:30:23 +0100

    Update dependencies and add NLTK downloader

---
+++ deep_learning/nltk_downloader.py
@@ -0,0 +1,8 @@
+# Helper file for installing NLTK stuff
+
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('averaged_perceptron_tagger')
